848b8abe062eb9492f3547d4fce7666db7563c63
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-07-18 14:58:29 +0200

    feat: update microservice-cli command description and add get command

--- cmd/root.go
+++ cmd/root.go
@@ -12,9 +12,8 @@ import (
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "microservice-cli",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+	Short: "Generate a basic template of microservice in Golang",
+	Long: `Generate template for microservice in Golang based on configuration file in yaml at the root of your project. For example:
 
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
