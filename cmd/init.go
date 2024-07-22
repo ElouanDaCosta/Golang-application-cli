df1fbb69b8d848b33bddf8e17c3ec49e03c78479
Elouan Da Costa Peixoto <elouandacostapeixoto@gmail.com>
2024-07-22 10:38:58 +0200

    feat: update the description of the list and init command, no example yet

--- cmd/init.go
+++ cmd/init.go
@@ -36,12 +36,7 @@ type promptContent struct {
 var generateCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Initialize a microservice app",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  `Initialize a microservice app using the specified technology between gin, gRPC or just basic http.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		appName, _ := cmd.Flags().GetString("name")
 
--- cmd/list.go
+++ cmd/list.go
@@ -15,13 +15,8 @@ import (
 // getCmd represents the get command
 var listCmd = &cobra.Command{
 	Use:   "list",
-	Short: "List all of generated app.",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "List generated app.",
+	Long:  `List all generated app or one app by name.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		appName, _ := cmd.Flags().GetString("name")
 
